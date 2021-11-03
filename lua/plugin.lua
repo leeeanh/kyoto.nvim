844fcea2bd049e231a0b89f961f8dc9fa29fcf8c
leeanh <leeanh3011@gmail.com>
2021-11-04 01:00:18 +0700

    fix: fix nvim-tree setup bug

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -57,7 +57,7 @@ require("packer").startup(function(use)
     "kyazdani42/nvim-tree.lua",
     requires = "kyazdani42/nvim-web-devicons",
     config = function()
-      require("nvim-tree").setup({})
+      require("plugins.nvimtree").setup()
     end,
   })
   use("glepnir/lspsaga.nvim")
